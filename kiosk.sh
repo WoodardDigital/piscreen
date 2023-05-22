aa6396d5d63f6faafed4da1dca2eb36841ca4d6e
Levi Woodard <92559124+WoodardDigital@users.noreply.github.com>
2023-05-21 23:15:37 -0600

    Create kiosk.sh
    added initial

---
+++ kiosk.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+
+chromium --noerrdialogs --disable-infobars --kiosk https://$URL-OF-YOUR-SYSTEM
+# Mine is behind NPM on my local network, it has a FQDN and SSL for  access. 
+
+sleep 20
+
+# The following section is for if you need to login on ever reboot.
+xdotool type '$UNIFI-USERNAME'
+sleep 1
+xdotool key Tab; sleep 1
+sleep 2
+xdotool type '$UNIFI-PASSWORD'
+# plaintext password, shoot me.... I'm sure it could be done better, but this is on my local network and for now it is what I am rolling with. 
+sleep 2
+xdotool key Return; sleep 2
+
+# The following auto refreshes the page every so many seconds
+while true; do
+   xdotool keydown ctrl+r; xdotool keyup ctrl+r;
+   sleep 10000
+done
+
+
+
+
