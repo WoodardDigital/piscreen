cc9a9fa66ca529a2feba5ce1b1c1be484291c8a8
Levi Woodard <92559124+WoodardDigital@users.noreply.github.com>
2023-05-22 10:20:19 -0600

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -10,6 +10,7 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chrom
 
 chromium --noerrdialogs --disable-infobars --kiosk https://$URL-OF-YOUR-SYSTEM
 # Mine is behind NPM on my local network, it has a FQDN and SSL for  access. 
+# The url is directly to the protect subpage of the view I want to access. not just the main protect page. 
 
 sleep 20
 
@@ -19,7 +20,7 @@ sleep 1
 xdotool key Tab; sleep 1
 sleep 2
 xdotool type '$UNIFI-PASSWORD'
-# plaintext password, shoot me.... I'm sure it could be done better, but this is on my local network and for now it is what I am rolling with. 
+# plaintext password, shoot me.... I'm sure it could be done better, but this is on my local network, has a limited unifi user with view only and for now it is what I am rolling with. 
 sleep 2
 xdotool key Return; sleep 2
 
